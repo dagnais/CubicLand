6f00cee627bde18c02e90bc71b3914b46c367bf2
dagnais <40569950+dagnais@users.noreply.github.com>
2019-04-27 18:52:05 -0300

    Player controller
    Inicial commit

---
+++ CenterMassController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterMassController : MonoBehaviour {
+    public bool isActive, isGround, isMassCenter;
+    public int id;
+    PJController _pjController;
+
+	void Start () {
+        _pjController = transform.parent.GetComponent<PJController>();
+	}
+	
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isActive)
+        {
+            _pjController.rb.velocity = Vector3.zero;
+            _pjController.rb.angularVelocity = Vector3.zero;
+            _pjController.rb.mass = 10;
+
+            for (int i = 0; i < _pjController.massCentersScript.Length; i++)
+            {
+                
+                if (_pjController.massCentersScript[i].isMassCenter)
+                    _pjController.massCentersScript[i].isGround = true;
+                else
+                {
+                    if (_pjController.massCentersScript[i].isActive)
+                        _pjController.massCentersScript[i].isGround = true;
+                    else
+                        _pjController.massCentersScript[i].isGround = false;
+                }
+                
+            }
+
+        }
+    }
+}
---
+++ PJController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PJController : MonoBehaviour {
+    public float speed=200, max=4;
+    public Transform[] massCenters;
+    public CenterMassController[] massCentersScript;
+    [HideInInspector]
+    public Rigidbody rb;
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.maxAngularVelocity = max;
+	}
+	
+	void FixedUpdate () {
+        rb.maxAngularVelocity = max;
+        Move();
+
+	}
+
+    void Move()
+    {
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+        int turn;
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            turn = 10;
+            rb.centerOfMass = massCenters[3].localPosition;
+            if (massCentersScript[1].isActive)
+            {
+                rb.mass = 1;
+                rb.angularVelocity = Vector3.zero;
+            }
+            massCentersScript[0].isActive = true;
+
+            massCentersScript[1].isActive = false;
+            massCentersScript[2].isActive = false;
+            massCentersScript[3].isActive = false;
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                turn = -10;
+                rb.centerOfMass = massCenters[2].localPosition;
+                if (massCentersScript[0].isActive)
+                {
+                    rb.mass = 1;
+                    rb.angularVelocity = Vector3.zero;
+                }
+
+                massCentersScript[1].isActive = true;
+
+                massCentersScript[0].isActive = false;
+                massCentersScript[2].isActive = false;
+                massCentersScript[3].isActive = false;
+            }
+            else
+                turn = 0;
+            
+        }
+        rb.AddTorque(transform.right * (speed*100) * turn, ForceMode.Impulse);
+    }
+}
